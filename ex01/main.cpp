03cb680b06589e47f57f58efb77a3137f0421339
jsollet <95758123+jsollet@users.noreply.github.com>
2023-03-07 16:29:05 +0100

    compil_test

---
+++ ex01/main.cpp
@@ -0,0 +1,88 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+int main()
+{
+	Bureaucrat::debug = false;
+
+
+	try
+	{
+		std::cout << "------------- out of range --------------- "<< std::endl;
+		std::cout<< "start:\t\t" << "Alfred = 0" << std::endl;
+		Bureaucrat	Alfred = Bureaucrat("Alfred", 0);
+	}
+	catch(std::exception &e)// standard
+	{// standard
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << "------------- out of range --------------- "<< std::endl;
+		std::cout<< "start:\t\t" << "Alfred = 151" << std::endl;
+		Bureaucrat	Alfred = Bureaucrat("Alfred", 151);
+	}
+	catch(std::exception &e)// standard
+	{// standard
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << "--------------- increment ---------------- "<< std::endl;
+		Bureaucrat	Alfred = Bureaucrat("Alfred", 3);
+		std::cout<< "start:\t\t" << Alfred << std::endl;
+		++Alfred;
+		std::cout<< "++increment:\t"<< Alfred << std::endl;
+		Alfred++;
+		std::cout<< "increment++:\t"<< Alfred << std::endl;
+		Alfred++;
+		std::cout<< "increment++:\t"<< Alfred << std::endl;
+	}
+	catch(std::exception &e)// standard
+	{// standard
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << "--------------- decrement ---------------- "<< std::endl;
+		Bureaucrat	Alfred = Bureaucrat("Alfred", 148);
+		std::cout<< "start:\t\t" << Alfred << std::endl;
+		--Alfred;
+		std::cout<< "--decrement:\t"<< Alfred << std::endl;
+		Alfred--;
+		std::cout<< "decrement--:\t"<< Alfred << std::endl;
+		Alfred--;
+		std::cout<< "decrement--:\t"<< Alfred << std::endl;
+	}
+	catch(std::exception &e)// standard
+	{// standard
+		std::cout << e.what() << std::endl;
+	}
+
+	try
+	{
+		std::cout << "----------------- ++/-- ------------------ "<< std::endl;
+		Bureaucrat	Alfred = Bureaucrat("Alfred", 100);
+		std::cout<< "start:\t\t" << Alfred << std::endl;
+		--Alfred;
+		std::cout<< "--decrement:\t"<< Alfred << std::endl;
+		Alfred--;
+		std::cout<< "decrement--:\t"<< Alfred << std::endl;
+		Alfred--;
+		std::cout<< "decrement--:\t"<< Alfred << std::endl;
+		++Alfred;
+		std::cout<< "++increment:\t"<< Alfred << std::endl;
+		Alfred++;
+		std::cout<< "increment++:\t"<< Alfred << std::endl;
+		Alfred++;
+		std::cout<< "increment++:\t"<< Alfred << std::endl;
+	}
+	catch(std::exception &e)// standard
+	{// standard
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
+}
